633e2dbc29daeb78da3e3a2107b8d36ba32afe4e
John B <jbyepez@gmail.com>
2022-10-18 11:45:25 -0500

    embed pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Buffer } from 'buffer';
+import { DomSanitizer } from '@angular/platform-browser';
+import { base64 as base64Pdf } from './base64';
+
+const url = URL.createObjectURL(new Blob([Buffer.from(base64Pdf, 'base64')], {type: 'application/pdf'}));
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'project-sample';
+  pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  pdfUrlFitW = this.sanitizer.bypassSecurityTrustResourceUrl(url + '#view=FitH');
+  dataUrl = this.sanitizer.bypassSecurityTrustResourceUrl('data:application/pdf;base64,' + base64Pdf + '#view=FitH');
+
+  constructor(public sanitizer: DomSanitizer) { }
 }
